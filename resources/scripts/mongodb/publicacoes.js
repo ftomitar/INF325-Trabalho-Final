6c30b431c2d2d3fd2e8ad43358a285ecb3852f9d
esoft12 - Felipe Iada Tomitar Vilar <felipeiada@gmail.com>
2021-09-03 12:13:51 -0300

    Mantendo apenas arquivos para trabalho

--- resources/scripts/mongodb/publicacoes.js
+++
@@ -1,9 +0,0 @@
-db = db.getSiblingDB('thedatasocietydb');
-
-// Obtendo todas as publicações
-var todasPublic = db.publicacoes.find();
-
-// Exibindo apenas os títulos das publicações
-while (todasPublic.hasNext()) {
-   print(todasPublic.next().titulo);
-}
